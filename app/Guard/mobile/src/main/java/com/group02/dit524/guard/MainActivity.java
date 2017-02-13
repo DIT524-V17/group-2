576fc229af8f6ba1520e8d92568e3a9875d465cd
Joacim Eberlen <guseberjo@student.gu.se>
2017-02-13 14:17:34 +0100

    Base android app, set up your own AVD

---
+++ app/Guard/mobile/src/main/java/com/group02/dit524/guard/MainActivity.java
@@ -0,0 +1,13 @@
+package com.group02.dit524.guard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
