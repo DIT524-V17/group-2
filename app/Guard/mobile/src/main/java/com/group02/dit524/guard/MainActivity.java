ae1bfc9410a9e37722706acfae783e77bcee0b75
Joacim Eberlen <guseberjo@student.gu.se>
2017-02-14 12:11:14 +0100

    The addition of a MainActivity with controls

--- app/Guard/mobile/src/main/java/com/group02/dit524/guard/MainActivity.java
+++ app/Guard/mobile/src/main/java/com/group02/dit524/guard/MainActivity.java
@@ -1,13 +1,39 @@
 package com.group02.dit524.guard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.icu.text.DecimalFormat;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
+    Control analogue;
+    TextView showMoveEvent;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showMoveEvent = (TextView) findViewById(R.id.showCoords);
+        analogue = (Control) findViewById(R.id.controlView1);
+        analogue.setOnMoveListener(new Control.OnMoveListener() {
+
+            @Override
+            public void onMoveInDirection(double polarAngle) {
+                if(polarAngle <= 0){
+                    double speed = analogue.getSpeed(100.0);
+                    showMoveEvent.setText("Max move in " + polarAngle + " direction. " + "\nSpeed: " + speed);
+                } else if(polarAngle > 0){
+                    double speed = analogue.getSpeed(-100.0);
+                    showMoveEvent.setText("Max move in " + polarAngle + " direction. " + "\nSpeed: " + speed);
+                }
+            }
+
+        });
     }
+
+
+
 }
