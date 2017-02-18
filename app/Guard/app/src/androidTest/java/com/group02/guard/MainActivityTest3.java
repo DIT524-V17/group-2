2280fdde2775c7589446189df91bad63f21b843f
Joacim Eberlen <guseberjo@student.gu.se>
2017-02-18 15:13:20 +0100

    The addition of a TESTREADME[WIP]

---
+++ app/Guard/app/src/androidTest/java/com/group02/guard/MainActivityTest3.java
@@ -0,0 +1,49 @@
+package com.group02.guard;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest3 {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest3() {
+        ViewInteraction button = onView(
+                allOf(withId(R.id.manualControlButton), withText("Manual Controls"),
+                        withParent(allOf(withId(R.id.activity_main),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        button.perform(click());
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.manualControlButton), withText("Manual Controls"),
+                        withParent(allOf(withId(R.id.activity_main),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        button2.perform(click());
+
+        pressBack();
+
+    }
+
+}
--- app/Guard/app/src/main/java/com/group02/guard/MainActivity.java
+++ app/Guard/app/src/main/java/com/group02/guard/MainActivity.java
@@ -10,8 +10,6 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
