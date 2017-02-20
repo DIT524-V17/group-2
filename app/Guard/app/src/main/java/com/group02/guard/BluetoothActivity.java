30405110e5d3c5f17b365e4f7acdc11c8a3c3d93
Joacim Eberlen <guseberjo@student.gu.se>
2017-02-20 09:57:10 +0100

    Commiting som minor changes

--- app/Guard/app/src/main/java/com/group02/guard/BluetoothActivity.java
+++ app/Guard/app/src/main/java/com/group02/guard/BluetoothActivity.java
@@ -6,25 +6,26 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class BluetoothActivity extends AppCompatActivity {
+
+    private BluetoothAdapter bluetoothAdapter = null;
+    private Set pairedDevices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
     }
 
 }
