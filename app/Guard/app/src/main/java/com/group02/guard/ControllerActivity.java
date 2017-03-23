19d38dc1aacad3c9237106686c25bd32209363d1
Joacim Eberlen <guseberjo@student.gu.se>
2017-03-23 10:59:20 +0100

    Done, battery and controller works.

--- app/Guard/app/src/main/java/com/group02/guard/ControllerActivity.java
+++ app/Guard/app/src/main/java/com/group02/guard/ControllerActivity.java
@@ -1,6 +1,12 @@
 package com.group02.guard;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -9,6 +15,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 import java.nio.ByteBuffer;
 
@@ -16,10 +23,18 @@ import static com.group02.guard.MainActivity.controlNav;
 
 public class ControllerActivity extends AppCompatActivity {
 
+    //Battery
+    // Following variables is used by the battery function
+    private Intent batteryStats;
+    private Bundle batteryBundle;
+    private Handler batteryHandler;
+    private ImageButton battery;
+    private double analogReadValue;
+    private double arduinoVoltage;
+    private boolean criticalLevel = false;
+
     Control analogue;
     TextView showMoveEvent;
-
-    ImageButton batteryButton;
 
     String address = "20:15:10:20:11:37";
 
@@ -28,14 +43,6 @@ public class ControllerActivity extends AppCompatActivity {
 
     // Handler for writing messages to the Bluetooth connection
     Handler writeHandler;
-
-
-
-    public static byte[] toByteArray(double value) {
-        byte[] bytes = new byte[8];
-        ByteBuffer.wrap(bytes).putDouble(value);
-        return bytes;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,27 +60,19 @@ public class ControllerActivity extends AppCompatActivity {
         * @purpose Initialize the battery button.
         * ToDo: Needs to be put into some where were it can be accessed.
         */
-        batteryButton = (ImageButton) findViewById(R.id.batteryButton);
-        final BatterySingleton battery = BatterySingleton.getInstance();
-        final Context context = this;
-        battery.initBattery(context);
-        battery.showBattery(batteryButton);
-        batteryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                battery.displayBatteryStats();
-            }
-        });
+        battery = (ImageButton) findViewById(R.id.batteryButton);
+        battery.setVisibility(View.VISIBLE);
 
         // Initialize the Bluetooth thread, passing in a MAC address
         // and a Handler that will receive incoming messages
+
         btt = new BluetoothThread(address, new Handler() {
 
             @Override
             public void handleMessage(Message message) {
 
             String s = (String) message.obj;
-            battery.readInput(s);
+            readInput(s);
 
             }
         });
@@ -91,16 +90,29 @@ public class ControllerActivity extends AppCompatActivity {
                 // [0] is left, [1] is right
                 int[] motors = analogue.motorSpeed((int)speed, (int)analogue.nAngle());
                 showMoveEvent.setText("Angle: " + analogue.nAngle() + "\nLEFT MOTOR: " + motors[0] + "\nRIGHT MOTOR: " + motors[1] + "." + "\nSpeed: " + speed);
+
+                if(motors[0] < 20){
+                    motors[0] += 20;
+                }else if(motors[1] < 20){
+                    motors[1] += 20;
+                }
+
                 write(motors[0], motors[1]);
             }
 
             @Override
             public void onMoveStopped() {
                 showMoveEvent.setText("Angle: " + analogue.nAngle() + "\nLEFT MOTOR: " + 0 + "\nRIGHT MOTOR: " + 0 + "." + "\nSpeed: " + 0);
-
                 write(0, 0);
             }
         });
+
+        batteryBundle = new Bundle();
+        batteryHandler = new Handler();
+        batteryStats = new Intent();
+        battery.setVisibility(View.VISIBLE);
+
+
     }
 
     public void write(int left, int right){
@@ -115,5 +127,113 @@ public class ControllerActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * @author Erik Laurin
+     * @purpose is to set battery icon depending on remaining battery level
+     */
+    public void setBatteryLevel() {
+        double voltage = getVoltage(analogReadValue);   //Converts from an analog value to voltage
+        voltage /=8; //To get average voltage for each battery
 
+        if(voltage >= 1.40) {   //Sets image depending on battery voltage = approx level based on alkaline AA discharge curve
+            battery.clearColorFilter();
+            battery.setImageResource(R.drawable.full_battery);
+            criticalLevel = false;
+        }
+        else if(voltage >= 1.30 && voltage < 1.40) {
+            battery.clearColorFilter();
+            battery.setImageResource(R.drawable.charged_battery);
+            criticalLevel = false;
+        }
+        else if(voltage >= 1.20 && voltage < 1.30) {
+            battery.clearColorFilter();
+            battery.setImageResource(R.drawable.half_charged_battery);
+            criticalLevel = false;
+        }
+        else if(voltage >= 1.05 && voltage < 1.20) {
+            battery.clearColorFilter();
+            battery.setImageResource(R.drawable.low_battery);
+            criticalLevel = false;
+        }
+        if(voltage < 1.05){
+            battery.setImageResource(R.drawable.empty_battery);
+            battery.setColorFilter(Color.RED);  //For effect
+            if(!criticalLevel) {
+                setCriticalBatteryLevelToast(); //Calls for toast
+                setCriticalBatteryLevelNotification();  //Calls for notification
+            }
+            criticalLevel = true;
+        }
+    }
+
+    /**
+     * @author Erik Laurin
+     * @purpose is to create a toast to notify the user of the SmartCar's critical battery level
+     */
+    private void setCriticalBatteryLevelToast(){
+        CharSequence text = "Critical battery level!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
+    }
+
+    /**
+     * @author Erik Laurin
+     * @purpose is to create a notification to notify the user of the SmartCar's critical battery level
+     */
+    private void setCriticalBatteryLevelNotification(){
+        NotificationCompat.Builder mBuilder= new NotificationCompat.Builder(this);
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        mBuilder.setSmallIcon(R.drawable.notification_battery)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.guard))
+                .setContentTitle("SmartCar Critical Battery Level")
+                .setAutoCancel(true)
+                .setContentText("content")
+                .setContentIntent(pendingIntent); //Sets the app to open MainActivity on press on notificaton
+        NotificationManager notificationManager= (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, mBuilder.build());
+    }
+
+    /**
+     * @author Erik Laurin
+     * @purpose is to calculate the SmartCar's battery voltage
+     * @param analogReadValue analogRead value from Arduino between 0-1024
+     * @return returns the SmartCar's battery voltage
+     */
+    private double getVoltage(double analogReadValue){
+        arduinoVoltage = analogReadValue* (5.0 / 1023.0); // Converts the analog reading to voltage
+        double voltage = arduinoVoltage * 5.0; //Restores the actual voltage measured (divided by 5 from the voltage divider before entiring the Arduino
+        return voltage;
+    }
+
+    /**
+     * @author Erik Laurin
+     * @purpose is to open a new View with battery stats when pressing the battery level indicator
+     */
+    public void displayBatteryStats(View view) {
+        batteryStats = new Intent(this, BatteryActivity.class);
+        batteryBundle = new Bundle();    //Sends intent extras in bundle
+        batteryBundle.putDouble("EXTRA_ANALOG", analogReadValue);
+        batteryBundle.putDouble("EXTRA_ARDUINO_VOLTAGE", arduinoVoltage);
+        batteryStats.putExtras(batteryBundle);
+        startActivity(batteryStats);
+    }
+
+    /**
+     * @author Erik Laurin
+     * @purpose refreshes the battery level indicator and values for the Battery activity
+     */
+
+    private void readInput(String s){
+        if(s.startsWith("B")){
+            try {
+                analogReadValue = Integer.parseInt(s.substring(1).trim());
+                setBatteryLevel();
+
+            } catch(NumberFormatException e) {
+                System.out.println("Could not parse " + "'" + s.substring(1) +"'");
+            }
+        }
+    }
 }
