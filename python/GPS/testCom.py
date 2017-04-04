c3af744418011a4152e6b3a684d171d6369f60f1
Erik <guslauer@student.gu.se>
2017-04-04 16:10:50 +0000

    increased performance testCom

--- python/GPS/testCom.py
+++ python/GPS/testCom.py
@@ -8,7 +8,7 @@ ser = serial.Serial('/dev/ttyACM0',9600) # '/dev/ttyUSB0'
 time.sleep(2)
 print(ser.name)  # check which port was really used
 while True:
-    ser.write(b'hello')  # write a string (prefix b is required for Python 3.x, converts it into bytes)
-    time.sleep(2)
+    ser.write(b'hello \n')  # write a string (prefix b is required for Python 3.x, converts it into bytes)
+    time.sleep(1)
 
 #ser.close()  # close port
