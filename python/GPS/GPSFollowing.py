9b770bbb3cd97df786e7691255ffa1d94007463b
Erik <guslauer@gu.student.se>
2017-04-21 00:50:38 +0200

    GPSfollowing sends the SmartCar's coordinates through a socket

--- python/GPS/GPSFollowing.py
+++ python/GPS/GPSFollowing.py
@@ -3,6 +3,7 @@ import time
 import serial
 import pynmea2
 import socket
+import boltons.socketutils
 from random import randrange, uniform
 from GpsAngle import calculateBearing, calculateDistance
 
@@ -26,51 +27,78 @@ sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
 # final variables
 a = 'A'
 s = 'S'
-new_line = '\n'
 update_frequency = 2
 
 # serials
-serialGPS = serial.Serial(port='/dev/ttyUSB0', baudrate=4800, timeout=None)
-# serialArduino = serial.Serial('/dev/ttyACM0', 9600)
+serial_GPS = serial.Serial(port='/dev/ttyUSB0', baudrate=4800, timeout=None)
+# serial_arduino = serial.Serial('/dev/ttyACM0', 9600)
 
-# GPS send
-serverName = 'localhost'
-serverPort = 8000
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
+# Variables for sending SmartCar GPS data
+send_server_name = 'localhost'
+send_server_port = 8000
+send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+send_socket.connect((send_server_name, send_server_port))
 
-class ThreadingGPSFollow():
+# Variables for retrieving phone GPS data
+receive_server_name = 'localhost'
+receive_server_port = 8001
+receive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+receive_socket.connect((receive_server_name, receive_server_port))
+buffered_receive_socket = boltons.socketutils.BufferedSocket(receive_socket, 0, 1024, )
 
-    def send_gps(self, message):
+# Phone Coordinates
+phone_longitude = ''
+phone_latitude = ''
+
+# SmartCar Coordinates
+smartcar_longitude = ''
+smartcar_latitude = ''
+old_angle = 0
+
+class threading_GPS_following():
+
+    def send_smartcar_coordinates(self):
         """
         Sends a message to the Java server (used for SmartCar's coordinates)
-        :param message: the string to be sent to the Java server
 
         Author Gabriel
         """
-        clientSocket.sendto(message.encode('utf-8'), (serverName, serverPort))
 
+        coordinates_smartcar = 'SC' + str(smartcar_latitude) + " " + str(smartcar_longitude) + "\n"
+        send_socket.sendto(coordinates_smartcar.encode('utf-8'), (send_server_name, send_server_port))
+
+    def get_phone_coordinates(self):
+        """
+        Receives the phone's coordinates through the specified socket and assign latitude and longitude their values
+        """
+        phone_coordinates = buffered_receive_socket.recv_until('\n')
+        if phone_coordinates.startswith('P'):
+            global phone_latitude
+            global phone_longitude
+            phone_coordinates_noprefix = phone_coordinates[1:]
+            phone_latitude, phone_longitude = phone_coordinates_noprefix.split()
+            print('phone_longitude: ' + phone_longitude)
+            print('phone_latitude: ' + phone_latitude)
 
     def send_angle(self, angle):
         """
         Sends angle to the Arduino
         :param angle: in degrees how much the SmartCar should turn
         """
-        byte_angle = str.encode(a + str(angle) + new_line)
-        print("Sends to arduino: " + byte_angle)
+        byte_angle = str.encode(a + str(angle) + '\n')
         # serialArduino.write(byte_angle)
-
+        print("Sent to arduino: " + byte_angle)
 
     def send_speed(self, speed):
         """
         Sends speed to the Arduino
         :param speed: the speed the SmartCar should be set to
         """
-        byte_speed = str.encode(s + str(speed) + new_line)
-        print("Sends to arduino: " + byte_speed)
+        byte_speed = str.encode(s + str(speed) + '\n')
         # serialArduino.write(byte_speed)
+        print("Sent to arduino: " + byte_speed)
 
-    def drive(self, angle, distance, pdop, fix):
+    def drive(self, pdop, fix):
         """
         Evaluates if the SmartCar should move, and if so, what speed it should have
         :param angle: the angle, in degrees, there is between the two different positions
@@ -82,6 +110,18 @@ class ThreadingGPSFollow():
             2 = DGPS
             3 = SPS
         """
+
+        global old_angle
+
+        distance = calculateDistance(smartcar_latitude, uniform(-90, 90), smartcar_longitude,
+                                     uniform(-180, 180)) * 1000  # Multiplied with 1000 to get m from km
+        angle = calculateBearing(smartcar_latitude, smartcar_longitude, uniform(-90, 90), uniform(-180, 180))
+
+        """ disregard minor changes in angle """
+        if abs(angle - old_angle) < 5:
+            return
+        else:
+            old_angle = angle
 
         """ if precision too poor - stops SmartCar and returns """
         if pdop > 6 and fix not in [1, 2, 3]:
@@ -99,7 +139,7 @@ class ThreadingGPSFollow():
         """ sends angle """
         self.send_angle(angle)
 
-        """ waits until the arduino has had enough time to turn """
+        """ waits until the SmartCar has had enough time to turn """
         if angle < 180:
             time.sleep(1)
         else:
@@ -121,43 +161,43 @@ class ThreadingGPSFollow():
     def run(self):
         """ Method that runs forever """
 
-        old_angle = 0
         pdop = 99
+        global smartcar_latitude
+        global smartcar_longitude
 
         while 1:
             try:
-                nmea_raw_data = serialGPS.readline()
+                """retrieve GPS coordinates from phone application"""
+                self.get_phone_coordinates()
+            except:
+                pass
+
+            try:
+                nmea_raw_data = serial_GPS.readline()
                 nmea_data = pynmea2.parse(nmea_raw_data)   # Parses into pynmea object ('nmea_raw_data, check=False' to disable checksum)
 
                 if isinstance(nmea_data, pynmea2.types.talker.GGA):
                     fix = nmea_data.gps_qual
 
                     if fix != 0:    # If GPS has a fix
-                        guard_latitude = nmea_data.latitude
-                        guard_longitude = nmea_data.longitude
+                        smartcar_latitude = nmea_data.latitude
+                        smartcar_longitude = nmea_data.longitude
 
-                        distance = calculateDistance(guard_latitude, uniform(-90, 90), guard_longitude, uniform(-180, 180)) * 1000 # Multiplied with 1000 to get m from km
-                        angle = calculateBearing(guard_latitude, guard_longitude, uniform(-90, 90), uniform(-180, 180))
+                        """ sends GPS coordinates to Java server (phone application)"""
+                        self.send_smartcar_coordinates()
 
-                        print("Latitude: " + str(guard_latitude))
-                        print("Longitude: " + str(guard_longitude))
+                        """ requests the SmartCar to start autonomous drive """
+                        self.drive(pdop, fix)
+
+                        print("guard_latitude: " + str(smartcar_latitude))
+                        print("guard_longitude: " + str(smartcar_longitude))
                         print("PDOP: " + str(pdop))
                         print("Fix quality: " + str(fix) + "\n")
-
-                        """ disregard minor changes in angle """
-                        if abs(angle - old_angle) < 3:
-                            continue
-                        else:
-                            old_angle = angle
-                            self.drive(angle, distance, pdop, 3)
-
-                        """ sends GPS info to Java server"""
-                        message = str(guard_latitude) + ":" + str(guard_longitude) + "\n"
-                        self.send_gps(message)
 
                     else:
                         print("Waiting for fix..")
                         print(nmea_raw_data)
+
                 elif isinstance(nmea_data, pynmea2.types.talker.GSA):
                     pdop = nmea_data.pdop
 
@@ -167,4 +207,4 @@ class ThreadingGPSFollow():
             except Exception as e:
                 print("Error caught: ", e)
 
-ThreadingGPSFollow()
+threading_GPS_following()
