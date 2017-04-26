f21235398a41a9a24da466824d63af8f225e778c
Erik <guslauer@gu.student.se>
2017-04-26 14:32:25 +0200

    set_coords_phone can now handle all numbers of arguments

--- GPSfollowing/set_coords_phone.py
+++ GPSfollowing/set_coords_phone.py
@@ -2,16 +2,31 @@ import sys
 
 def write_to_file(argv):
     try:
+
         coords = open('/home/pi/repo/group-2/GPSfollowing/coords_phone.txt', "w", -1 )
-        phone_latitude = argv[1]
-        phone_longitude = argv[2]
 
-        message = str(phone_latitude) + ' ' + str(phone_longitude)
-        coords.write(message)
+        if len(argv) == 3:
+            phone_latitude = argv[1]
+            phone_longitude = argv[2]
 
-        confirm_message = 'phone_latitude set to: ' + str(phone_latitude) + '\n' + 'phone_longitude set to: ' + str(phone_longitude)
-        print(confirm_message)
+            message = str(phone_latitude) + ' ' + str(phone_longitude)
+            coords.write(message)
 
+            confirm_message = 'phone_latitude set to: ' + str(phone_latitude) + '\n' + 'phone_longitude set to: ' + str(phone_longitude)
+            print(confirm_message)
+
+        elif len(argv) == 2:
+            phone_latitude = argv[1]
+            phone_longitude = '0'
+
+            message = str(phone_latitude) + ' ' + str(phone_longitude)
+            coords.write(message)
+
+            confirm_message = 'phone_latitude set to: ' + str(phone_latitude) + '\n' + 'phone_longitude set to: ' + str(phone_longitude)
+            print(confirm_message)
+        else:
+            print('Wrong amount of arguments. 2 (lat long) or 1 (error code) required')
+            return
     except:
         pass
 
