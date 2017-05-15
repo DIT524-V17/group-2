ef6138bb4d996544da52a0000f9635a270bbac79
justinas <gusstirju@student.gu.se>
2017-05-15 20:38:27 +0200

    updated work items for consistency

--- node.js/guardRest.js
+++ node.js/guardRest.js
@@ -13,8 +13,8 @@ REST_ROUTER.prototype.handleRoutes = function(router, pool) {
 	//Adds new Traveller to DB
     router.post("/travellers", Auth.BasicAuthentication, function(req, res){
         var query = "INSERT INTO travellers (email, password, admin_status) VALUES (?, ?, 0)";
-        var table = [req.body.email, req.body.password];
-        query = mysql.format(query, table);
+        var params = [req.body.email, req.body.password];
+        query = mysql.format(query, params);
 
         //Getting a connection from the pool
         pool.getConnection(function(err, connection){
@@ -61,8 +61,8 @@ REST_ROUTER.prototype.handleRoutes = function(router, pool) {
     //Gets specific Traveller from DB	
     router.get("/travellers/:email", Auth.BasicAuthentication, function(req, res){
         var query = "SELECT * FROM travellers WHERE email = ?";
-        var table = [req.params.email];
-        query = mysql.format(query, table);
+        var params = [req.params.email];
+        query = mysql.format(query, params);
 
         //Getting a connection from the pool
         pool.getConnection(function(err, connection){
@@ -82,12 +82,36 @@ REST_ROUTER.prototype.handleRoutes = function(router, pool) {
         });
     });
 
+    //Command 'GET'
+    //Gets all values from the connectivity table
+    router.get("/connectivity", Auth.BasicAuthentication, function(req, res){
+        var query = "SELECT * FROM connectivity";
+        query = mysql.format(query);
+
+        //Getting a connection from the pool
+        pool.getConnection(function(err, connection){
+            connection.query(query, function(err, rows){
+                //Putting the connection back in the pool for later reuse
+                connection.release();
+                //JSON response
+                if(err) {
+                    res.json({"Error" : true,
+                    "Message" : "Error while getting connection info"});
+                } else {
+                    res.json({"Error" : false,
+                    "Message" : "Connection info received",
+                    "Info" : rows});
+                }
+            });
+        });
+    });
+
     //Command 'PUT'
     //Updates Traveler's password for specific email
     router.put("/travellers", Auth.BasicAuthentication, function(req, res){
         var query = "UPDATE travellers SET password = ? WHERE email = ?";
-        var table = [req.body.password, req.body.email];
-        query = mysql.format(query, table);
+        var params = [req.body.password, req.body.email];
+        query = mysql.format(query, params);
 
         //Getting a connection from the pool
         pool.getConnection(function(err, connection){
@@ -107,11 +131,11 @@ REST_ROUTER.prototype.handleRoutes = function(router, pool) {
     });
 
     //Command 'PUT'
-    //Updates Traveler's email based on password
+    //Updates Traveler's email based on user_id
     router.put("/travellers/:user_id", Auth.BasicAuthentication, function(req, res){
         var query = "UPDATE travellers SET email = ? WHERE user_id = ?";
-        var table = [req.body.email, req.params.user_id];
-        query = mysql.format(query, table);
+        var params = [req.body.email, req.params.user_id];
+        query = mysql.format(query, params);
 
         //Getting a connection from the pool
         pool.getConnection(function(err, connection){
@@ -130,12 +154,36 @@ REST_ROUTER.prototype.handleRoutes = function(router, pool) {
         });
     });
 
+    //Command 'PUT'
+    //Updates SmartCars coordinates
+    router.put("/cars", Auth.BasicAuthentication, function(req, res){
+        var query = "UPDATE guard_cars SET latitude = ?, longitude = ? WHERE car_id = ?";
+        var params = [req.body.latitude, req.body.longitude, req.body.car_id];
+        query = mysql.format(query, params);
+
+        //Getting a connection from the pool
+        pool.getConnection(function(err, connection){
+            connection.query(query, function(err, rows){
+                //Putting the connection back in the pool for later reuse
+                connection.release();
+                //JSON response
+                if(err) {
+                    res.json({"Error" : true,
+                    "Message" : "Error while updating coordinates"});
+                } else {
+                    res.json({"Error" : false,
+                    "Message" : "Coordinates updated"});
+                }
+            });
+        });
+    });
+
     //Command 'DELETE'
 	//Deletes specific Traveller based on email
     router.delete("/travellers", Auth.BasicAuthentication, function(req, res){
         var query = "DELETE FROM travellers WHERE email = ?";
-        var table = [req.body.email];
-        query = mysql.format(query, table);
+        var params = [req.body.email];
+        query = mysql.format(query, params);
 
         //Getting a connection from the pool
         pool.getConnection(function(err, connection){
