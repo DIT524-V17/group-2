111a19c8cb25da5f74b3085418737f680279e2d0
Erik <guslauer@gu.student.se>
2017-05-23 15:54:55 +0200

    removed menu icon for toolbar and unavailable message for the battery indicator

--- app/Guard/app/src/main/java/com/group02/guard/BatteryActivity.java
+++ app/Guard/app/src/main/java/com/group02/guard/BatteryActivity.java
@@ -2,34 +2,43 @@ package com.group02.guard;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 /**
  * The class creates an activity that displays detailed battery data
  *
  * @author Erik Laurin
- * @version 1.0.0
+ * @version 1.0.1
  */
 
 public class BatteryActivity extends AppCompatActivity {
-    TextView analogValue;
-    TextView arduinoVoltage;
-    TextView actualVoltage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battery);
 
-        Bundle b = getIntent().getExtras();
+        TextView analogValue;
+        TextView arduinoVoltage;
+        TextView actualVoltage;
+
+        Bundle batteryBundle = getIntent().getExtras();
+
         analogValue = (TextView) findViewById(R.id.analogValue);
         arduinoVoltage = (TextView) findViewById(R.id.arduinoVoltage);
         actualVoltage = (TextView) findViewById(R.id.actualVoltage);
 
-        analogValue.setText("Arduino analog read value: " + (b.getDouble("EXTRA_ANALOG")));
-        arduinoVoltage.setText("Arduino voltage input: " + String.format("%.3f",
-                (b.getDouble("EXTRA_ARDUINO_VOLTAGE"))) + "V");
-        actualVoltage.setText("Estimated battery pack voltage: " + String.format("%.3f",
-                (b.getDouble("EXTRA_ARDUINO_VOLTAGE"))*5.0) + "V");
+        if (batteryBundle.getDouble("EXTRA_ANALOG")==0.000){
+            analogValue.setText("Battery details not yet available");
+            arduinoVoltage.setVisibility(View.GONE);
+            actualVoltage.setVisibility(View.GONE);
+        }else{
+            analogValue.setText("Arduino analog read value: " + (batteryBundle.getDouble("EXTRA_ANALOG")));
+            arduinoVoltage.setText("Arduino voltage input: " + String.format("%.3f",
+                    (batteryBundle.getDouble("EXTRA_ARDUINO_VOLTAGE"))) + "V");
+            actualVoltage.setText("Estimated battery pack voltage: " + String.format("%.3f",
+                    (batteryBundle.getDouble("EXTRA_ARDUINO_VOLTAGE"))*5.0) + "V");
+        }
     }
 }
--- app/Guard/app/src/main/java/com/group02/guard/ToolbarTopFragment.java
+++ app/Guard/app/src/main/java/com/group02/guard/ToolbarTopFragment.java
@@ -23,7 +23,6 @@ import android.widget.Toast;
 public class ToolbarTopFragment extends Fragment {
 
     Toolbar toolbar;
-    ImageButton menuButton;
     ImageButton batteryButton;
     TextView appTitle;
 
@@ -41,17 +40,9 @@ public class ToolbarTopFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.toolbar_top, parent, false);
         this.toolbar = (Toolbar) view.findViewById(R.id.top_toolbar);
-        this.menuButton = (ImageButton) view.findViewById(R.id.menuButton);
         this.batteryButton = (ImageButton) view.findViewById(R.id.batteryButton);
         this.appTitle = (TextView) view.findViewById(R.id.toolbarTitle);
         appTitle.setText("G.U.A.R.D");
-
-        menuButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showOptionMenu(v);
-            }
-        });
 
         batteryButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -61,50 +52,6 @@ public class ToolbarTopFragment extends Fragment {
         });
 
         return view;
-    }
-
-    /**
-     * Creates a popup option menu
-     * @param v Current View
-     */
-    public void showOptionMenu(View v) {
-        PopupMenu popup = new PopupMenu(getActivity(), v);
-        MenuInflater inflater = popup.getMenuInflater();
-        inflater.inflate(R.menu.menu_option, popup.getMenu());
-        popup.show();
-    }
-
-    /**
-     * Identifies selected option from the option menu by comparing their ids
-     * @param item The clicked option in the pop up menu
-     * @return true For the selected option
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.nav_profile:
-                Toast.makeText(getActivity(), "Check back in future sprints", Toast.LENGTH_SHORT)
-                        .show();
-                return true;
-            case R.id.nav_settings:
-                Toast.makeText(getActivity(), "Check back in future sprints", Toast.LENGTH_SHORT)
-                        .show();
-                return true;
-            case R.id.nav_help:
-                Toast.makeText(getActivity(), "Check back in future sprints", Toast.LENGTH_SHORT)
-                        .show();
-                return true;
-            case R.id.nav_feedback:
-                Toast.makeText(getActivity(), "Check back in future sprints", Toast.LENGTH_SHORT)
-                        .show();
-                return true;
-            case R.id.nav_logout:
-                Toast.makeText(getActivity(), "Check back in future sprints", Toast.LENGTH_SHORT)
-                        .show();
-                return true;
-            default:
-                return false;
-        }
     }
 
     /**
